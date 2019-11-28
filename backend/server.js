903ae249aea44f88a1974553fa9b5f4a89d4de40
rmoreira <lidnele@hotmail.com>
2019-11-28 11:08:39 -0300

    test

--- backend/server.js
+++ backend/server.js
@@ -19,10 +19,13 @@ app.use(bodyParser.json());
 
 
 // Create link to Angular build directory
-app.use(express.static(__dirname + '/../dist'));
+app.use(express.static(__dirname + '../dist'));
 
 if(process.env.MONGOURL ){
-    mongoose.connect(process.env.MONGOURL);
+    mongoose.connect(process.env.MONGOURL,{ useNewUrlParser: true, useUnifiedTopology: true  });
+}
+else{
+    mongoose.connect("mongodb://localhost:27017",{ useNewUrlParser: true, useUnifiedTopology: true  })
 }
 const connection = mongoose.connection;
 connection.once('open', () => {
